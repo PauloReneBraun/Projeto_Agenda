9e94173584f2f0172e072684e61476e17565375f
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-07-18 20:44:30 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#131016",
+      }}
+    >
+      color: #FDFCFE, fonSize: 20, FontWeight: 'bold',
+      <Text>Paulo Braun</Text>
+    </View>
+  );
+}
